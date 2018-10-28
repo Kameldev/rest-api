53f9795fed4627ef1286230b7ef0df7d4c9b312e
Kamel REMAKI <remakikamel@hotmail.fr>
2018-10-28 09:48:59 +0100

    create ArticleController

---
+++ src/AppBundle/Controller/ArticleController.php
@@ -0,0 +1,27 @@
+<?php
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Article;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use FOS\RestBundle\Controller\ControllerTrait;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+
+
+class  ArticleController extends  AbstractController
+{
+
+
+
+
+
+
+
+
+
+
+
+}
